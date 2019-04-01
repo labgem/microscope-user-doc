c88ffdf1d2b0edaa151e1d3c667389c4641b7295
Mathieu Dubois <mdubois@genoscope.cns.fr>
2019-04-01 14:06:27 +0200

    Update copyright.

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MicroScope User Doc'
-copyright = u'2015'
+copyright = u'2019'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
