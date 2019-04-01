3902d88a5178cb309e45610bbaadd3d7c23f566a
Mathieu Dubois <mdubois@genoscope.cns.fr>
2019-04-01 14:06:47 +0200

    Disable pngmath extension as it seems to break compilation.

--- docs/conf.py
+++ docs/conf.py
@@ -25,8 +25,17 @@ version = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
-              'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    # https://github.com/rtfd/readthedocs.org/issues/2418
+    #'sphinx.ext.pngmath',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['templates']
